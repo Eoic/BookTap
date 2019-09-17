8f3df925fcd569323d3a6cad7f08a849f7747a00
Eoic <strazdas.karolis@outlook.com>
2019-09-17 17:15:18 +0300

    Fix npm scripts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,25 @@
+import dotenv from "dotenv";
+const dotConfig = dotenv.config();
 import express from "express";
+import "reflect-metadata";
 import { routes } from "./api/helpers/routeDistributor";
 import { useMiddleware, useRoutes } from "./utilities/bootstrap";
 import { connection } from "./utilities/database";
 import middleware from "./utilities/middleware";
 
 const router = express();
+const port = process.env.SERVER_PORT;
 
 connection.then(() => {
     console.log("DB connection successful");
     useMiddleware(middleware, router);
     useRoutes(routes, router);
 }).then(() => {
-    router.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
+    router.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
     });
 }).catch((err) => {
     console.log(err);
 });
+
+console.log(process.env.NODE_ENV);
