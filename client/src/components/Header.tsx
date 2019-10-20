3e17006ae0493f60acd823788ae055ca4f973ab9
Eoic <strazdas.karolis@outlook.com>
2019-10-20 10:31:31 -0700

    Add feature descriptions in landing page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,12 +7,10 @@ export function Header(props: IHeaderProps) {
     return (
         <header className="hero">
             <div className="hero-content">
-                <h1> Your online book library </h1>
+                <h1> Your online e-book library </h1>
                 <div className="hero-header-divider">&nbsp;</div>
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p> Easy to use e-book management system to organize your books into digital bookshelves, grouping 
+                    and sorting by custom topics and metadata. </p>
             </div>
             <img alt="Pile of books." style={{ maxWidth: "37%" }} src="img/book-pile.png"></img>
         </header>
--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -9,27 +9,33 @@ export default class Main extends React.Component<IMainProps> {
       <main className="features-list">
         <div className="card">
             <h4>
-            <i className="fas fa-book-open"></i> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
+            <i className="fas fa-globe"></i> Access from any device </h4>
+            <p> It does not matter wheter you are using PC, mobile device or a tablet </p>
         </div>
         <div className="card">
-            <h4> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
-        </div>        <div className="card">
-            <h4> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
+            <h4> 
+            <i className="fas fa-file-alt"></i> Different e-book formats supported </h4>
+            <p> Upload books in .pdf, .epub, .mobi, .djvu formats </p>
+        </div>        
+        <div className="card">
+            <h4>
+            <i className="fas fa-tasks"></i> Track your progress </h4>
+            <p> Mark which books you read, currently reading or already finished </p>
         </div>
         <div className="card">
-            <h4> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
+            <h4> 
+            <i className="fas fa-random"></i> Organize into categories </h4>
+            <p> Separate yout books into bookshelves, assign topics, descriptions or sort by metadata </p>
         </div>
         <div className="card">
-            <h4> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
+            <h4> 
+            <i className="fas fa-database"></i> Convenient file transfer </h4>
+            <p> Download your books easily to any device in bulk as an archive or separately one file at a time </p>
         </div>
         <div className="card">
-            <h4> Feature title </h4>
-            <p> lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm lorem ipusm </p>
+            <h4> 
+            <i className="fas fa-tablet-alt"></i> Built-in reader </h4>
+            <p> Open and read books in the browser without installing additional software </p>
         </div>
       </main>
     );
